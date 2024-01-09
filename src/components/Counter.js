dc74ad348f8b34322ffb835bcf2dab50c4f54e16
fahim-abser <fahimabser0123@gmail.com>
2024-01-09 23:42:28 +0600

    Constant & action setup ready

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
+import React  from 'react';
 
 const Counter = () => {
-    const [count, setCount]=useState(2)
+    
     const handleIncrease = ()=>{
-        setCount((count)=>count+1);
+        
     }
     const handleDecrease = ()=>{
-        setCount((count)=>count-1);
+       
     }
     return (
         
         
         <div>
             <h1>Counter App</h1>
-            <h3>Count: {count}</h3>
+            <h3>Count: 0</h3>
             <button onClick={handleIncrease}>Increment</button>
             <button onClick={handleDecrease}>Decrement</button>
         </div>
---
+++ src/services/Action/ActionCounter.js
@@ -0,0 +1,17 @@
+import { DECREMENT, INCREMENT, RESET } from "../Constants/Constants"
+
+export const incrementCounter=()=>{
+    return{
+        type: INCREMENT
+    }
+}
+export const decrementCounter=()=>{
+    return{
+        type: DECREMENT
+    }
+}
+export const resetCounter=()=>{
+    return{
+        type: RESET
+    }
+}
